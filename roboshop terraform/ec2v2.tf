44c3069c57bf68b18f28d5fdc2527315c4634ff2
Nazrina <nazrinakhan88@gmail.com>
2025-03-29 18:04:05 -0400

    ec2 instance

--- roboshop terraform/ec2v2.tf
+++ roboshop terraform/ec2v2.tf
@@ -8,6 +8,14 @@ resource "aws_instance" "frontend" {
   }
 }
 
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z03035903COPLEGQ5980F"
+  name    = "frontend"
+  type    = "A"
+  ttl     = 3
+  records = [aws_instance.frontend.private_ip]
+}
+
 resource "aws_instance" "mongo" {
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t2.micro"
@@ -18,6 +26,14 @@ resource "aws_instance" "mongo" {
   }
 }
 
+resource "aws_route53_record" "mongo" {
+  zone_id = "Z03035903COPLEGQ5980F"
+  name    = "mongo"
+  type    = "A"
+  ttl     = 3
+  records = [aws_instance.mongo.private_ip]
+}
+
 resource "aws_instance" "catalogue" {
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t2.micro"
@@ -26,4 +42,12 @@ resource "aws_instance" "catalogue" {
   tags = {
     Name = "catalogue"
   }
+}
+
+resource "aws_route53_record" "catalogue" {
+  zone_id = "Z03035903COPLEGQ5980F"
+  name    = "catalogue"
+  type    = "A"
+  ttl     = 3
+  records = [aws_instance.catalogue.private_ip]
 }
