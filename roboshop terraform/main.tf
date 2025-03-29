cc9d82bf493197046bd20281a45b724ad77a9ce7
Nazrina <nazrinakhan88@gmail.com>
2025-03-29 19:51:05 -0400

    ec2 instance

--- roboshop terraform/main.tf
+++ roboshop terraform/main.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "frontend" {
   ami           = data.aws_ami.ami.image_id
   instance_type = var.instance_type
-  vpc_security_group_ids = data.aws_security_group_ids.sg.ids
+  vpc_security_group_ids = data.aws_security_groups.sg.ids
 
 
   tags = {
@@ -21,7 +21,7 @@ resource "aws_route53_record" "frontend" {
 resource "aws_instance" "mongo" {
   ami           = data.aws_ami.ami.image_id
   instance_type = var.instance_type
-  vpc_security_group_ids = data.aws_security_group_ids.sg.ids
+  vpc_security_group_ids = data.aws_security_groups.sg.ids
 
   tags = {
     Name = "mongo"
@@ -39,7 +39,7 @@ resource "aws_route53_record" "mongo" {
 resource "aws_instance" "catalogue" {
   ami           = data.aws_ami.ami.image_id
   instance_type = var.instance_type
-  vpc_security_group_ids = data.aws_security_group_ids.sg.ids
+  vpc_security_group_ids = data.aws_security_groups.sg.ids
 
   tags = {
     Name = "catalogue"
