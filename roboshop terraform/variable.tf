f1f3eebd449b17fa99a54f0a5f99c532216345fc
Nazrina <nazrinakhan88@gmail.com>
2025-03-29 19:46:48 -0400

    ec2 instance

--- roboshop terraform/variable.tf
+++ roboshop terraform/variable.tf
@@ -7,9 +7,9 @@ variable "instance_type" {
 #     default = ["sg-09b81f0daabd7b47a"]
 # }
 
-variable "zone_id" {
-    default = "Z03035903COPLEGQ5980F"
-}
+# variable "zone_id" {
+#     default = "Z03035903COPLEGQ5980F"
+# }
 
 variable "domain_name" {
     default =  "naifah.online"
