9fa02ad2e58dd5829e905a6d591045dcaa792f55
Nazrina <nazrinakhan88@gmail.com>
2025-03-29 01:30:20 -0400

    ec2 instance

--- roboshop terraform/ec2v2.tf
+++ roboshop terraform/ec2v2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "frontend" {
   ami           = "ami-09c813fb71547fc4f"
-  instance_type = "t3.micro"
+  instance_type = "t2.micro"
   vpc_security_group_ids = ["sg-09b81f0daabd7b47a"]
 
   tags = {
@@ -10,7 +10,7 @@ resource "aws_instance" "frontend" {
 
 resource "aws_instance" "mongo" {
   ami           = "ami-09c813fb71547fc4f"
-  instance_type = "t3.micro"
+  instance_type = "t2.micro"
   vpc_security_group_ids = ["sg-09b81f0daabd7b47a"]
 
   tags = {
@@ -20,7 +20,7 @@ resource "aws_instance" "mongo" {
 
 resource "aws_instance" "catalogue" {
   ami           = "ami-09c813fb71547fc4f"
-  instance_type = "t3.micro"
+  instance_type = "t2.micro"
   vpc_security_group_ids = ["sg-09b81f0daabd7b47a"]
 
   tags = {
