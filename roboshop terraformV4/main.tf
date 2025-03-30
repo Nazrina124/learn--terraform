3f7c033c88c9d6b790ef79542400a4eb4d581efb
Nazrina <nazrinakhan88@gmail.com>
2025-03-30 17:53:00 -0400

    ec2 instance

--- roboshop terraformV4/main.tf
+++ roboshop terraformV4/main.tf
@@ -10,22 +10,10 @@ resource "aws_instance" "instances" {
     Name = "${each.key}.dev"
   }
 
-provisioner "remote-exec" {
-  connection {
-    user = "ec2-user"
-    password ="DevOps321"
-    host = self.private_ip
-
-    }
-
-   inline = [
-    "sudo pip-3.11 install ansible",
-    "ansible-pull -i localhost, -U https://github.com/Nazrina412/Ansible-Robo main.yml -e env=dev -e role_name=${each.key}"
-    ]
   }
 }
 
-resource "aws_route53_record" "frontend" {
+resource "aws_route53_record" "dns_record" {
     for_each = var.components
   zone_id = data.aws_route53_zone.zone.zone_id
   name    = "${each.key}.dev.${var.domain_name}"
@@ -34,3 +22,23 @@ resource "aws_route53_record" "frontend" {
   records = [aws_instance.instances[each.key].private_ip]
 }
 
+
+resource "null_resource" "ansible" {
+    depends_on = [aws_route53_record.dns_record]
+    for_each = var.components
+    provisioner "remote-exec" {
+      connection {
+        user = "ec2-user"
+        password ="DevOps321"
+        host = [aws_instance.instances[each.key].private_ip]
+
+        }
+
+       inline = [
+        "sudo pip-3.11 install ansible",
+        "ansible-pull -i localhost, -U https://github.com/Nazrina412/Ansible-Robo main.yml -e env=dev -e role_name=${each.key}"
+        ]
+
+   }
+
+}
