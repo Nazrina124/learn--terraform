4f7cd3b5fe0c2ab0fdb0f77d0ab7135a9a5c7fda
Nazrina <nazrinakhan88@gmail.com>
2025-03-30 14:21:48 -0400

    ec2 instance

--- moudel1/ec2/main.tf
+++ moudel1/ec2/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "frontend" {
-  ami           = data.aws_ami.ami.image_id
-  instance_type = var.instance_type
-  vpc_security_group_ids = data.aws_security_groups.sg.ids
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = "sg-09b81f0daabd7b47a"
 
 
   tags = {
